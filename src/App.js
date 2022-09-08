8edda8b6fe13f279547670403b0bd347ff9937b7
diah pramesti <dpanbers@gmail.com>
2022-09-08 12:52:05 +0800

    new edit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import { Container, Row } from 'react-bootstrap';
 import './App.css';
+import Form from './component/Form';
+import ListStudent from './component/list-student';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [studentList, setStudentList] = useState([])
+
+  useEffect(() => {
+    localStorage.setItem('student list', JSON.stringify(studentList));
+  }, [studentList])
+  
+  useEffect(() => {
+    const getStudent = localStorage.getItem('studentList')
+    if(getStudent) {
+      setStudentList(JSON.parse(getStudent))
+    }
+  }, [])
+
+  return(
+    <Container>
+      <h1 style={{ marginTop: 10, textAlign: 'center', color: 'pink'}}>Student List in Hacktiv8</h1>
+      <Row>
+        <h3 style={{ marginTop: 20, marginBottom: 10}}>Form to Add New</h3> 
+        <Form setStudentList={setStudentList} studentList={studentList}/>
+      </Row>
+
+      <ListStudent setStudentList={setStudentLists} studentList={studentList}/>
+
+    </Container>  
+  )
 }
 
 export default App;
